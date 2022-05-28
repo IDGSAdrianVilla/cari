4833624bfce5d59b70fff494c92c960b4316cd92
Adrian_Villa <villa.isc.tec@gmail.com>
2022-05-28 07:23:06 -0500

    Correción env

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -65,16 +65,11 @@ class ComposerAutoloaderInit0418fc2ad416711d0726e174a8d45d78
     }
 }
 
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
 function composerRequire0418fc2ad416711d0726e174a8d45d78($fileIdentifier, $file)
 {
     if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
         require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
